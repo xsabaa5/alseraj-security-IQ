37812fcd411bf2c2e29ef4fd7075a73e26538a78
sabaa <sabaa13seraj@gmail.com>
2026-02-09 09:18:55 +0300

    fix: remove min-height from Google Map container for better layout

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -129,7 +129,7 @@ export default function ContactUs() {
         </div>
 
         {/* Google Map */}
-        <div className="rounded-xl overflow-hidden border border-white/10 min-h-screen">
+        <div className="rounded-xl overflow-hidden border border-white/10">
           <iframe
             title="Al Seraj Location"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3323.8!2d44.439563!3d33.313688!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x15577e1041f7e58d%3A0xe56039b4e51c4480!2sAl%20Seraj%20Co.%20for%20Information%20Technology!5e0!3m2!1sen!2siq!4v1700000000000!5m2!1sen!2siq"
