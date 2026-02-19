a770d27184b342236ffbc18485acd1322a680dc1
sabaa <sabaa13seraj@gmail.com>
2026-02-19 14:45:09 +0300

    .

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -14,7 +14,6 @@ const navLinks = [
   { key: "nav.home", hash: "home" },
   { key: "nav.about", hash: "about" },
   { key: "nav.services", hash: "services", hasDropdown: true },
-  { key: "nav.solution", hash: "solutions" },
   { key: "nav.contactUs", to: "/contact" },
 ];
 
